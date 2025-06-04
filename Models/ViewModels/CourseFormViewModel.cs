215a344ff7eabf1d43aac43fddf6d28fae013ede
SarahSaman <shoshaaman@gmail.com>
2025-06-04 12:56:42 +0300

    courseformvm

---
+++ Models/ViewModels/CourseFormViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineCourseRegistration_FatemaSarah.Models.ViewModels
+{
+    public class CourseFormViewModel
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int Capacity { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(1, 52)]
+        public int Duration { get; set; }
+
+        [Required]
+        public int InstructorId { get; set; }
+
+        public IFormFile? PdfFile { get; set; }
+
+        public List<SelectListItem> Instructors { get; set; } = new();
+    }
+}
---
+++ Models/ViewModels/CourseStudentCountViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineCourseRegistration_FatemaSarah.Models.ViewModels
+{
+    public class CourseStudentCountViewModel
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int StudentCount { get; set; }
+    }
+}
